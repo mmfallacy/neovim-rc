2e5a907dcb0efb675342f2060b3abfbfb22d5c49
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-02-02 01:59:19 +0800

    remaps

--- lua/mmfallacy/keybinds.lua
+++ lua/mmfallacy/keybinds.lua
@@ -1,11 +1,33 @@
 local noremap = { noremap = true }
 local silent_noremap = {noremap = true, silent = true}
-vim.g.mapleader = " " 
+local map = vim.keymap.set
 
 -- Normal Remaps
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+map("n", "<leader>pv", vim.cmd.Ex)
+map("i", "<C-c>", "<Esc>")
+
+-- Buffer Remaps (ful1e5)
+map('n', '<space>]', vim.cmd.bnext)
+map('n', '<space>[', vim.cmd.bprev)
+map('n', '<space><Esc>', vim.cmd.bdelete)
+map('n', '<C-d>', ':%bd!<CR>')
+
+-- Line Transforms using Alt (ful1e5)
+map('n', '<A-S-j>', ':m .+1<CR>==')
+map('n', '<A-S-k>', ':m .-2<CR>==')
+map('v', '<A-S-j>', ":m '>+1<CR>gv=gv")
+map('v', '<A-S-k>', ":m '<-2<CR>gv=gv")
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 
 -- Telescope Remaps
 local t = require'telescope.builtin'
-vim.keymap.set('n', '<leader>ff', t.find_files, silent_noremap)
-vim.keymap.set('n', '<C-p>', t.git_files, silent_noremap)
+map('n', '<leader>ff', t.find_files, silent_noremap)
+map('n', '<C-p>', t.git_files, silent_noremap)
+
+-- Disable Arrow Keys
+map({'n','v','i'}, "<Left>", "<nop>")
+map({'n','v','i'}, "<Right>", "<nop>")
+map({'n','v','i'}, "<Down>", "<nop>")
+map({'n','v','i'}, "<Up>", "<nop>")
+
