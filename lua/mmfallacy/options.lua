de66aa0c1276eeab68887e5254998c355fb248e8
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-02-02 01:59:35 +0800

    nvim options

--- lua/mmfallacy/options.lua
+++ lua/mmfallacy/options.lua
@@ -1,1 +1,86 @@
+local g = vim.g
+local o = vim.o
+
+-- Globals
+g.mapleader = ' '
+g.netrw_liststyle = 3
+
+-- Clipboard
 vim.opt.clipboard = 'unnamedplus'
+
+-- Interface
+o.mouse = 'n'
+o.ruler = true
+o.wrap = false
+o.scrolloff = 8
+o.termguicolors = true
+o.linebreak = true
+o.signcolumn = 'number'
+o.nu = true
+o.relativenumber = true
+o.rnu = true
+o.updatetime = 50
+
+-- Indents
+o.tabstop = 2
+o.shiftwidth = 2
+o.softtabstop = 2
+o.expandtab = true
+o.smartindent = true
+
+-- Search
+o.hlsearch = false
+o.incsearch = true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
