be7c640f3706d9ea8458e454af5e2485bd7fdaa9
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-03-01 11:22:46 +0800

    change git files to find files for config

--- lua/mmfallacy/keybinds.lua
+++ lua/mmfallacy/keybinds.lua
@@ -1,10 +1,10 @@
 local noremap = { noremap = true }
-local silent_noremap = {noremap = true, silent = true}
+local silent_noremap = { noremap = true, silent = true }
 local map = vim.keymap.set
 
 -- Normal Remaps
 map("n", "<leader>pv", vim.cmd.Ex)
-map({"n","v","c","i"}, "<C-c>", "<Esc>")
+map({ "n", "v", "c", "i" }, "<C-c>", "<Esc>")
 map("n", "<C-a>", "ggVG")
 
 -- Buffer Remaps (ful1e5)
@@ -21,15 +21,14 @@ map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 
 -- Telescope Remaps
-local t = require'telescope.builtin'
+local t = require 'telescope.builtin'
 map('n', '<leader>fg', t.git_files, silent_noremap)
 map('n', '<leader>fr', t.live_grep, silent_noremap)
 map('n', '<C-p>', t.find_files, silent_noremap)
-map('n', '<C-,>', function() t.git_files({ cwd = vim.fn.stdpath('config') }) end, silent_noremap)
+map('n', '<C-,>', function() t.find_files({ cwd = vim.fn.stdpath('config') }) end, silent_noremap)
 map('n', '<leader>ft', t.treesitter, silent_noremap)
 map('n', '<leader>fb', t.buffers, silent_noremap)
 
 -- Sync Arrow Keys to HJKL
-map({'n','v'}, "<Left>", "<nop>")
-map({'n','v'}, "<Right>", "<nop>")
-
+map({ 'n', 'v' }, "<Left>", "<nop>")
+map({ 'n', 'v' }, "<Right>", "<nop>")
