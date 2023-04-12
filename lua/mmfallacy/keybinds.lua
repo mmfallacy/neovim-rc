b13c3a3908b798efa58a9d679058b5b7ef608aaf
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-04-12 20:48:44 +0800

    feat: add keybind for buffer delete

--- lua/mmfallacy/keybinds.lua
+++ lua/mmfallacy/keybinds.lua
@@ -5,11 +5,12 @@ local map = vim.keymap.set
 -- Normal Remaps
 map("n", "<leader>pv", vim.cmd.Ex)
 map({ "n", "v", "c", "i" }, "<C-c>", "<Esc>")
-map("n", "<C-a>", "ggVG")
+-- map("n", "<C-a>", "ggVG")
 
 -- Buffer Remaps (ful1e5)
 map('n', '<leader>]', vim.cmd.bnext)
 map('n', '<leader>[', vim.cmd.bprev)
+map('n', '<leader>bd', ":bd!<CR>")
 
 -- Line Transforms using Alt (ful1e5)
 
@@ -25,7 +26,7 @@ local t = require 'telescope.builtin'
 map('n', '<leader>fg', t.git_files, silent_noremap)
 map('n', '<leader>fr', t.live_grep, silent_noremap)
 map('n', '<C-p>', t.find_files, silent_noremap)
-map('n', '<C-,>', function() t.find_files({ cwd = vim.fn.stdpath('config') }) end, silent_noremap)
+map('n', '<C-o>', function() t.find_files({ cwd = vim.fn.stdpath('config') }) end, silent_noremap)
 map('n', '<leader>ft', t.treesitter, silent_noremap)
 map('n', '<leader>fb', t.buffers, silent_noremap)
 
