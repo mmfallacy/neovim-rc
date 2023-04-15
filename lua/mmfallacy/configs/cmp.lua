12626f545d495fa0c976a14676b7d6990a405aef
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-04-16 01:39:00 +0800

    feat: keybind for scrolling docs

--- lua/mmfallacy/configs/cmp.lua
+++ lua/mmfallacy/configs/cmp.lua
@@ -15,7 +15,8 @@ return function()
             ['<Down>'] = cmp.mapping.close(),
             ['<S-Tab>'] = cmp.mapping.select_prev_item(),
             ['<Tab>'] = cmp.mapping.select_next_item(),
-            ['<C-e>'] = cmp.mapping.abort(),
+            ['<C-Up>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-Down>'] = cmp.mapping.scroll_docs(4), ['<C-e>'] = cmp.mapping.abort(),
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<CR>'] = cmp.mapping.confirm {
                 select = true,
